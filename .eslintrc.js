30de7afc665cd27d284929f443b380a152654898
Tid Huang <yesseecity@yahoo.com.tw>
2023-10-04 23:59:45 +0800

    Add libary: eslint-config-airbnb-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -17,10 +19,13 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    // for NestJS default sylte
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-duplicates':  'off',
     'max-len': ['error', {
       'code': 110, 
       'ignoreUrls': true, 

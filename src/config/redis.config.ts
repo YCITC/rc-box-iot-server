c340401ab1f35111930dd371ddee58a26cdebb91
Tid Huang <yesseecity@yahoo.com.tw>
2024-01-20 23:07:23 +0800

    Refactor main.ts

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,8 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('REDIS', () => ({
-  config: {
-    host: 'localhost',
-    port: 6379,
-  },
+  // host: 'redis',
+  port: 6379,
 }));

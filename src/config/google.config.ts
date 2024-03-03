0dc2b0ce5217c4c2048e9ae64939de70a6178f1f
Tid Huang <yesseecity@yahoo.com.tw>
2024-03-03 22:33:44 +0800

    Change Google OAuth secret

--- src/config/google.config.ts
+++ src/config/google.config.ts
@@ -6,9 +6,8 @@ export default registerAs('google', () => ({
     APIKey: 'AIzaSyCUsr3UrfNUNoP7KYR5h5eoKt0DUjQjBDQ',
   },
   OAuth2: {
-    clientID:
-      '633936908307-atiu6bpu8iaf7uf2jk4abllm7b2s77sj.apps.googleusercontent.com',
-    clientSecret: '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',
+    clientID: 'google_oauth2_client_id',
+    clientSecret: 'google_oauth2_client_id',
     callbackRoute: '/oauth-redirect',
   },
 }));
